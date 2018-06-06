81058db7c0f90140a4f0cfa9ab423ab8eb71c171
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2018-06-06 16:50:41 -0300

    Start Project

---
+++ GameDesignProject/Assets/Scripts/CastGrid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastGrid : MonoBehaviour
+{
+    //Variables
+    public float gridSize = 6f;
+    private Vector2 lastPosition = Vector2.zero;
+
+    //While Mouse is in Area
+    private void OnMouseOver()
+    {
+        Vector2 currentPosition = Input.mousePosition;
+        currentPosition = Camera.main.ScreenToWorldPoint(currentPosition);
+        currentPosition = currentPosition - (Vector2) this.transform.position;
+
+        if((currentPosition - lastPosition).magnitude >= 0.5f)
+        {
+            //Get Angle
+            Vector2 atVector = currentPosition - lastPosition;
+            float ang = Mathf.Atan2(atVector.y, atVector.x) * Mathf.Rad2Deg;
+
+            //Debug.Log("FROM: " + lastPosition + " TO: " + currentPosition + " ANGLES: " + ang);
+
+
+
+
+
+
+
+
+            //Update Last Position
+            lastPosition = currentPosition;
+        }
+    }
+
+    //If Mouse Exit Area
+    void OnMouseExit()
+    {
+        Destroy(this.gameObject);
+    }
+}
