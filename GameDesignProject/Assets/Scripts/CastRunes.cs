11271e90404f2c23f92be3e0a9084386f287fe53
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2018-06-14 03:22:53 -0300

    Master Rune Revamp V0.1
    Master Rune Revamp
    Map Dimensions Adjusted

---
+++ GameDesignProject/Assets/Scripts/CastRunes.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastRunes : MonoBehaviour
+{
+    //Spheres
+    public GameObject[] spheresList;
+
+    //Singleton
+    private static CastRunes instance;
+
+    //On Object Awake
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    //On Object Destroy (Safeguard)
+    public void OnDestroy()
+    {
+        instance = null;
+    }
+
+    //If Mouse Exit Area
+    void OnMouseExit()
+    {
+        resetCastRunes();
+    }
+
+    //Reset CastRunes Method
+    public void resetCastRunes()
+    {
+        //Reset Spheres
+        for(int i = 0; i < spheresList.Length; i++)
+        {
+            spheresList[i].GetComponent<RuneSphere>().resetSphere();
+        }
+
+        //Disable CastRunes
+        gameObject.SetActive(false);
+    }
+
+    
+}
---
+++ GameDesignProject/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //CastRunes Reference
+    public CastRunes castRunes;
+
+    //On Mouse Click
+    private void OnMouseDown()
+    {
+        castRunes.gameObject.SetActive(true);
+    }
+
+    //Update Method
+    private void Update()
+    {
+        //Reset CastRunes if Mouse is Released
+        if(Input.GetMouseButtonUp(0) && castRunes.gameObject.activeInHierarchy) castRunes.resetCastRunes();
+    }
+}
