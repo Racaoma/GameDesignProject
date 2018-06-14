419cc82352d6d03312b20b6df83a23210db7d2f0
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2018-06-13 22:26:26 -0300

    Aula 13/06

---
+++ GameDesignProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    protected int hitPoints;
+    protected float speed;
+}
